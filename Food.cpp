6573810dddfcfca438fd3c533541124761bda4bb
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-07-15 16:01:51 -0500

    Food class

---
+++ Food.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "food.h"
+
+Food::Food(int board[ROWS][COLUMNS]) {
+  COORD rand;
+  do {
+    rand.x = std::rand() % (COLUMNS - 1) + 1;
+    rand.y = std::rand() % (ROWS - 1) + 1;
+  } while(board[rand.y][rand.x] != ' ');
+  this->p = rand;
+}
+
+COORD Food::get_p() const {
+  return this->p;
+}
