2ce4574391f976014e6ea245a342e8c0ad92632d
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-09-14 10:29:19 -0500

    Board

--- ScoreBoard.cpp
+++ ScoreBoard.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <fstream>
-// #include <bits/stdc++.h>
-
+#include <bits/stdc++.h>
+using namespace std;
+#include "Player.h"
 #include "ScoreBoard.h"
 
-
-ScoreBoard::ScoreBoard() : file_name(".score") {
+ScoreBoard::ScoreBoard() {
   read_top_players();
+}
+
+ScoreBoard::~ScoreBoard() {
+  save_players();
 }
 
 std::vector<Player> ScoreBoard::get_top_players() const {
@@ -14,7 +18,13 @@ std::vector<Player> ScoreBoard::get_top_players() const {
 }
 
 void ScoreBoard::add_top_player(Player player) {
-  top_players.push_back(player);
+  Player a("gg", 180);
+  print_players();
+  this->top_players.at(0) = a;
+  std::cout << std::endl;
+  print_players();
+
+  // top_players.push_back(player);
 }
 
 void ScoreBoard::print_players() const {
@@ -23,30 +33,29 @@ void ScoreBoard::print_players() const {
 }
 
 void ScoreBoard::read_top_players() {
-  std::ifstream score_file(file_name);
-  std::string name;
+  std::ifstream score_input(filename);
   int score;
+  std::string name;
 
-  while(score_file.is_open() && score_file >> name >> score) {
+  while(score_input.is_open() && score_input >> name >> score) {
     Player tmp(name, score);
     top_players.push_back(tmp);
   } 
-  sort();
+  // sort();
 }
 
 void ScoreBoard::save_players() const {
-  std::ofstream score_file(file_name, std::ofstream::out | std::ofstream::trunc);
-  for(int i = 0; i < top_players.size(); i++) {
+  std::ofstream score_file(filename, std::ofstream::out | std::ofstream::trunc);
+  for(int i = 0; i < top_players.size(); i++)
     score_file << top_players[i];
-  }
 }
 
 void ScoreBoard::sort() {
-  print_players();
-  std::cout << std::endl;
-  std::vector<Player> players = this->top_players;
-  players[0] = players[5];
-  players[5] = players[2];
-  this->top_players = players;
-  print_players();
+  // print_players();
+  // std::cout << std::endl;
+  // std::vector<Player> players = this->top_players;
+  // players[0] = players[5];
+  // players[5] = players[2];
+  // this->top_players = players;
+  // print_players();
 }
--- ScoreBoard.h
+++ ScoreBoard.h
@@ -4,16 +4,20 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <bits/stdc++.h>
 
 #include "Player.h"
 
 class ScoreBoard {
   private: 
     std::vector<Player> top_players;
-    const char *file_name;
+    const std::string filename = ".score";
+    const int max_len = 20;
+    // std::ofstream score_output;
     
   public:
     ScoreBoard();
+    ~ScoreBoard();
     std::vector<Player> get_top_players() const;
     void add_top_player(Player);
     void print_players() const;
